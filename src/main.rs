b0e6c13034b78139f3a8bcbfac190682290187e6
F-a-z-i <fazecka@gmail.com>
2024-11-07 01:16:10 -1100

    Fix changes before running cargo fix

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 mod chapter03;
 mod chapter04;
+mod chapter05;
+mod chapter06;
+mod chapter07;
+mod chapter08;
+mod chapter09;
+mod chapter10;
+mod chapter11;
+mod chapter12;
+mod cristmas_tree;
+mod envelope;
+mod gcd;
+mod hackerrank;
+mod invert_the_case;
+mod is_palindrome;
+mod is_prime;
+mod rectangles;
+mod rhombus;
+mod string_rotate;
 
 fn main() {
     println!("Hello, world!");
---
+++ target/debug/deps/rust_221on-e4e91c7b2215f822.d
@@ -0,0 +1,25 @@
+C:\Users\aser\RustroverProjects\rust-221on\target\debug\deps\rust_221on-e4e91c7b2215f822.exe: src/main.rs src\chapter03.rs src\chapter04.rs src\chapter05.rs src\chapter06.rs src\chapter07.rs src\chapter08.rs src\chapter09.rs src\chapter10.rs src\chapter11.rs src\chapter12.rs src\cristmas_tree.rs src\envelope.rs src\gcd.rs src\hackerrank.rs src\invert_the_case.rs src\is_palindrome.rs src\is_prime.rs src\rectangles.rs src\rhombus.rs src\string_rotate.rs
+
+C:\Users\aser\RustroverProjects\rust-221on\target\debug\deps\rust_221on-e4e91c7b2215f822.d: src/main.rs src\chapter03.rs src\chapter04.rs src\chapter05.rs src\chapter06.rs src\chapter07.rs src\chapter08.rs src\chapter09.rs src\chapter10.rs src\chapter11.rs src\chapter12.rs src\cristmas_tree.rs src\envelope.rs src\gcd.rs src\hackerrank.rs src\invert_the_case.rs src\is_palindrome.rs src\is_prime.rs src\rectangles.rs src\rhombus.rs src\string_rotate.rs
+
+src/main.rs:
+src\chapter03.rs:
+src\chapter04.rs:
+src\chapter05.rs:
+src\chapter06.rs:
+src\chapter07.rs:
+src\chapter08.rs:
+src\chapter09.rs:
+src\chapter10.rs:
+src\chapter11.rs:
+src\chapter12.rs:
+src\cristmas_tree.rs:
+src\envelope.rs:
+src\gcd.rs:
+src\hackerrank.rs:
+src\invert_the_case.rs:
+src\is_palindrome.rs:
+src\is_prime.rs:
+src\rectangles.rs:
+src\rhombus.rs:
+src\string_rotate.rs:
